76aa2ed78b72549ce809248f40f823334e2f29d2
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-14 12:36:42 +0530

    Array: Create Duplicate Element In An Array

---
+++ Arrays/CreateDuplicateElementsInArray.js
@@ -0,0 +1,8 @@
+// Create duplicate elements in an Array
+function duplicates(arr){
+    return arr.concat(arr);
+}
+
+
+// console.log(duplicates([1, 2, 3]));
+console.log(duplicates([1, 2, 3, 4, 5]));
